f65763507d3d3071d928b9edcc1cd5db8bff8ac6
Terogene Claudio <cterogene@gmail.com>
2021-10-19 22:30:40 -0800

    Add files via upload

---
+++ DetailActivity.java
@@ -0,0 +1,91 @@
+package com.example.flixter;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixter.models.Movie;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.parceler.Parcels;
+
+import okhttp3.Headers;
+
+public class DetailActivity extends YouTubeBaseActivity {
+
+    private static final String YOUTUBE_API_KEY= "AIzaSyABui5rkqdqy8Ih1sOjtKS6zSzRe72AxbY";
+    public  static  final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=60a82fccdbb2f79a2d57ba56e2ad2bed";
+    TextView tvTitle;
+    TextView tvOverview;
+    RatingBar ratingBar;
+    TextView popularity;
+    YouTubePlayerView youTubePlayerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        tvTitle = findViewById(R.id.tvTitle);
+        tvOverview = findViewById(R.id.tvOverview);
+        ratingBar = findViewById(R.id.ratingBar);
+        popularity = findViewById(R.id.popularity);
+        youTubePlayerView = findViewById(R.id.player);
+
+        Movie movie = Parcels.unwrap(getIntent().getParcelableExtra("movie"));
+        tvTitle.setText(movie.getTitle());
+        tvOverview.setText(movie.getOverview());
+        ratingBar.setRating((float) movie.getRating());
+        popularity.setText("Popularity: " + movie.getPopularity());
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(String.format(VIDEOS_URL, movie.getMovieId()), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                try {
+                    JSONArray results = json.jsonObject.getJSONArray("results");
+
+                    if(results.length()==0){
+                        return;
+                    }
+
+                    String youtubeKey = results.getJSONObject(0).getString("key");
+                    Log.d("DetailActivity", youtubeKey);
+                    initializeYoutube(youtubeKey);
+                }catch (JSONException e){
+                    Log.e("DetailActivity",  "Failed to parse JSON", e);
+
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+            }
+        });
+
+
+    }
+
+    private void initializeYoutube(final String youtubeKey) {
+
+        youTubePlayerView.initialize(YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                Log.d("DetailActivity", "onInitializationSuccess");
+                youTubePlayer.cueVideo(youtubeKey);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.d("DetailActivity", "onInitializationFailure");
+            }
+        });
+    }
+}
---
+++ Movie.java
@@ -0,0 +1,70 @@
+package com.example.flixter.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Parcel
+public class Movie {
+    int movieId;
+    String backdropPath;
+    String posterPath;
+    String title;
+    String Overview;
+    double rating;
+    String popularity;
+
+    //empty constructor needed by the Parceler library
+    public Movie(){}
+
+    public Movie(JSONObject jsonObject) throws JSONException{
+        backdropPath = jsonObject.getString("backdrop_path");
+        posterPath = jsonObject.getString("poster_path");
+        title      = jsonObject.getString("title");
+        Overview   = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
+        popularity = jsonObject.getString("popularity");
+
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException{
+        List<Movie> movies = new ArrayList<>();
+        for(int i=0; i<movieJsonArray.length(); i++){
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s",posterPath);
+    }
+
+    public String getBackdropPath(){
+        return String.format("https://image.tmdb.org/t/p/w342/%s",backdropPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return Overview;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public String getPopularity() {
+        return popularity;
+    }
+}
